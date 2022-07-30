baa6f407a95c2ece0ed51dcda9643d9b6c18c866
Dmytro Skorobohatskyi <skorobogatskiidima@gmail.com>
2022-07-31 00:39:27 +0300

    Added CatalogController class. Added dependency injection on context and repository interfaces.

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -13,21 +13,7 @@ namespace Catalog.API
     {
         public static void Main(string[] args)
         {
-            //float grn_dollar = 36.57f;
-            //float sum_to_send = 1060f;
-            //float percent_to_sub = 0.5f + 0.25f + 2f;
-            //Console.WriteLine("Summa(grn) to send - " + grn_dollar * sum_to_send);
-            ////Console.WriteLine("Summa(grn) received - " + (grn_dollar * sum_to_send - (grn_dollar * sum_to_send) * percent_to_sub / 100f));
-            //float received_sum_with_percent_applied_grn = (grn_dollar * sum_to_send - (grn_dollar * sum_to_send) * percent_to_sub / 100f);
-            //Console.WriteLine("Summa(grn) received - " + received_sum_with_percent_applied_grn);
-
-            //float dollar_by_grn_buy = 37.037f;
-            //float dollar_to_lear = 17.5f;
-            //float real_lear_to_grn = 2.08f;
-            ////Console.WriteLine("Summa(dollar) received - " + received_sum_with_percent_applied_grn / dollar_by_grn_buy);
-            //Console.WriteLine("Lears to take wishdraw - " + received_sum_with_percent_applied_grn / real_lear_to_grn);
-
-            ////CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
